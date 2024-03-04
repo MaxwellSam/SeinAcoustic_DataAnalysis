ede26d4b5db6cf928ba2434921cf755f7d129fd4
Sam Maxwell <maxwellsamjm@gmail.com>
2024-03-04 17:44:27 +0100

    aggreg done

--- DBBuilder/dbbuilder.py
+++ DBBuilder/dbbuilder.py
@@ -8,52 +8,74 @@ from IPython.display import display
 
 class DBBuilder (DateReader):
 
-    def __init__(self, latitude:float=48.886, longitude:float=2.333, elevation:float=35, timezone:str="UTC", timefreq="min", **kargs) -> None:
+    timefreq_to_count_occ:str="min"
+
+    def __init__(self, latitude:float=48.886, longitude:float=2.333, elevation:float=35, timezone:str="UTC", timefreq="h", **kargs) -> None:
         self.sun = Sun(latitude=latitude, longitude=longitude, elevation=elevation, timezone=timezone, **kargs)
         self.moon = Moon(latitude=latitude, longitude=longitude, elevation=elevation, timezone=timezone, **kargs)
         self.meteo = OpenMeteo(latitude=latitude, longitude=longitude, timezone=timezone)
         self.timefreq = timefreq
         super().__init__(timezone=timezone)
         
-
-    def create (self, dates:np.ndarray, labels:np.ndarray, format:str="%Y-%m-%d %H:%M:%S"):
-        labels_occ = self.get_labels_occurrence(dates=dates, labels=labels, format=format)
-        print("labels_occ")
-        display(labels_occ)
-        df_sun = pd.DataFrame(labels_occ.date.apply(self.sun.get_infos, format=format).to_list())
-        print("df_sun")
-        display(df_sun)
-        df_moon = pd.DataFrame(labels_occ.date.apply(self.moon.get_infos, format=format).to_list())
-        print("df_moon")
-        display(df_moon)
+    def create_acoustic_db (self, dates:np.ndarray, labels:np.ndarray, format:str="%Y-%m-%d %H:%M:%S"):
+        # 1) Count labels occurence over time  
+        labels_count = self.count_labels_occurence(dates=dates, labels=labels, timefreq=self.timefreq_to_count_occ, format=format)
+        # 2) Find astral informations
+        ## 2.1) Sun informations
+        df_sun = pd.DataFrame(labels_count.date.apply(self.sun.get_infos, format=format).to_list())
+        ## 2.2) Moon informations
+        df_moon = pd.DataFrame(labels_count.date.apply(self.moon.get_infos, format=format).to_list())
         astral_infos = pd.merge(df_sun, df_moon, on="date")
-        print("astral_infos")
-        display(astral_infos)
-        final_df = pd.merge(labels_occ, astral_infos, on="date")
-        print("final_df")
-        display(final_df)
-        return final_df
-        
+        labels_count = pd.merge(labels_count, astral_infos, on="date")
+        # 3) Agregation by suncycles
+        labels_count_aggBy = self.agregate_count_by(df=labels_count, timefreq=self.timefreq, format=format)
+        return labels_count_aggBy 
+    
+    def agregate_count_by (self, df:pd.DataFrame, traget_cols:list=None, col_date:str="date", grpBy=["suncycle_type", "suncycle_day"], 
+                              timefreq="h", format="%Y-%m-%d %H:%M:%S", traget_prefix:str="label"):
+        """
+        Agregate a set of count (expl: label counts over time) by time frequency and other (default suncycle info)
+        ## Params
+        df: pd.DataFrame
+            dataframe with counting informations and other (suncycle info)
+        target_cols: list
+            columns to aggregate (must be a counting measure)
+        col_date: str
+            column name of date values
+        grpBy: list
+            column name to group by (default by suncycle type and day)
+        timefreq: str
+            time frequency for aggregation (default hourly)
+        format: str
+            datetime format if str
+        target_prefix: str
+            prefix of target columns if target columns list is None
+        # Return  
+        aggreg: pd.DataFrame
+            results of aggregation
+        """
+        df[col_date] = df[col_date].apply(self.read_date, format=format)
+        traget_cols = [c for c in df.columns if c.startswith(traget_prefix)] if not traget_cols else traget_cols
+        grpby_params = [df[col_date].dt.floor(timefreq)] + grpBy 
 
-        
-    def get_labels_occurrence (self, dates:np.ndarray, labels:np.ndarray, timefreq="min", format="%Y-%m-%d %H:%M:%S"):
+        agg_params = {k:"sum" for k in traget_cols}
+        agg_params.update({col_date: ['min', 'max']})
+        aggreg = df.groupby(grpby_params).agg(agg_params).reset_index()
+        aggreg.columns = ['_'.join(col) for col in aggreg.columns.values]
+        return aggreg
+
+    def count_labels_occurence (self, dates:np.ndarray, labels:np.ndarray, timefreq="min", prefix:str="label_", format="%Y-%m-%d %H:%M:%S"):
         """
         Count labels occurrence in a time frequency from a list of dates and labels
         """
         df = pd.DataFrame({"date":dates, "label":labels})
         df.date = df.date.apply(self.read_date, format=format)
-        df.date = df.date.apply(self.read_date, format=format)
-        df['date'] = pd.to_datetime(df['date'])
         df_grouped = df.groupby([pd.Grouper(key='date', freq=timefreq), 'label']).size().reset_index(name='count')
-        print("df_grouped")
-        display(df_grouped)
         df_pivot = df_grouped.pivot_table(index='date', columns='label', values='count', fill_value=0)
-        print("df_pivot")
-        display(df_pivot)
         index_complet = pd.date_range(start=df_pivot.index.min(), end=df_pivot.index.max(), freq=timefreq)
-        df_final = df_pivot.reindex(index_complet, fill_value=0)
-        df_final = df_final.add_prefix("label_").reset_index(names=["date"])
-        # df_final.columns = [f"label_{col}" if col != 'date' else col for col in df_final.columns]
-        return df_final
+        labels_count = df_pivot.reindex(index_complet, fill_value=0)
+        labels_count = labels_count.add_prefix(prefix).reset_index(names=["date"])
+        # labels_count.columns = [f"label_{col}" if col != 'date' else col for col in labels_count.columns]
+        return labels_count
 
 
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import pandas as pd
 dbbuilder = DBBuilder()
 files = glob.glob("./data/bougival/acoustique/acoustique SENSEA/yolo/*.csv", recursive=True)
 df = pd.read_csv(files[0])
-db = dbbuilder.create(dates=df.date, labels=df.label)
+db = dbbuilder.create_acoustic_db(dates=df.date, labels=df.label)
